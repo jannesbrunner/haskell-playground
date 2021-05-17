c5d15a48372305092496a4d41f8efb915e053d41
Jannes Brunner <jannes.brunner@oo-services.com>
2021-05-17 18:47:01 +0200

    Refactore caesar funcs

--- module2.hs
+++ module2.hs
@@ -1,3 +1,4 @@
+import Data.Char
 {--
 Content:
 
@@ -6,28 +7,27 @@ Content:
 
 --}
 
--- Casar-Chiffre
+-- Casar-Chiffre (shifting in ASCII Table)
 encryptCaesar :: Int -> [Char] -> [Char]
-encryptCaesar key word = map (\x -> toEnum $ fromEnum x + key) word
+encryptCaesar key = map (\x -> toEnum $ fromEnum x + key)
 
 decryptCaesar :: Int -> [Char] -> [Char]
-decryptCaesar key word = map (\x -> toEnum $ fromEnum x - key) word
+decryptCaesar key = map (\x -> toEnum $ fromEnum x - key)
 
--- Vignere Chiffre
+-- Vignere Chiffre (bound to a=0 .. z=25)
 
-asciiCapitalLetter x = 65 <= x && x <= 90  
+asciiCapitalLetter x = 65 <= x && x <= 90
 asciiSmallLetter x = 97 <= x && x <= 122
 toSmallLetter x = if asciiSmallLetter x then x else x + 32
 toAlphaIndex a = if asciiSmallLetter (fromEnum a) then (fromEnum a) - 97 else (fromEnum a) - 65
 
 
 encryptVigenere :: [Char] -> [Char] -> [Char]
-encryptVigenere word key = [encrypt x y | (x, y) <- zip word (cycle  key)]
+encryptVigenere word key = [encrypt x y | (x, y) <- zip word (cycle key)]
 
 encrypt x y | asciiSmallLetter   (fromEnum x) = toEnum (mod (toAlphaIndex x + toAlphaIndex y) 26 + 97)
             | asciiCapitalLetter (fromEnum x) = toEnum (mod (toAlphaIndex x + toAlphaIndex y) 26 + 65)
             | otherwise = x
- 
 decryptVigenere :: [Char] -> [Char] -> [Char]
 decryptVigenere word key = [decrypt x y | (x, y) <- zip word (cycle  key)]
 
