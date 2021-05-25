ff9ff3bee656652cbdc69685e8b3b8b88faee3ca
Jannes Brunner <jannes.brunner@oo-services.com>
2021-05-25 18:27:02 +0200

    Re-Structure code

--- module2.hs
+++ module2.hs
@@ -6,14 +6,26 @@ Content:
 
 --}
 
+-- Helper Functions
+
 groupFive :: [Char] -> [Char]
 groupFive x = if length x > 5 then take 5 x ++ [' '] ++ groupFive (drop 5 x) else x
 
+filterWhitespace :: [Char] -> [Char]
+filterWhitespace = filter (\a -> fromEnum a /= 32)
+
+asciiCapitalLetter x = 65 <= x && x <= 90
+asciiSmallLetter x = 97 <= x && x <= 122
+toSmallLetter x = if asciiSmallLetter x then x else x + 32
+toAlphaIndex a = if asciiSmallLetter (fromEnum a) then fromEnum a - 97 else fromEnum a - 65
 
 -- Casar-Chiffre
 
 encryptCaesar :: Int -> [Char] -> [Char]
 encryptCaesar key word = groupFive (map (caesarizer True key) (filterWhitespace word))
+
+decryptCaesar :: Int -> [Char] -> [Char]
+decryptCaesar key word = map (caesarizer False key) (filterWhitespace word)
 
 caesarizer up key x | up && asciiCapitalLetter (fromEnum x) = toEnum (mod (toAlphaIndex x + key) 26 + 65)
                     | up && asciiSmallLetter (fromEnum x) = toEnum (mod (toAlphaIndex x + key) 26 + 97)
@@ -21,29 +33,18 @@ caesarizer up key x | up && asciiCapitalLetter (fromEnum x) = toEnum (mod (toAlp
                     | not up && asciiSmallLetter (fromEnum x) = toEnum ( mod (toAlphaIndex x - key) 26 + 97)
                     | otherwise = x
 
-decryptCaesar :: Int -> [Char] -> [Char]
-decryptCaesar key word = map (caesarizer False key) (filterWhitespace word)
-
 
 -- Vignere Chiffre (bound to a=0 .. z=25)
 
-asciiCapitalLetter x = 65 <= x && x <= 90
-asciiSmallLetter x = 97 <= x && x <= 122
-toSmallLetter x = if asciiSmallLetter x then x else x + 32
-toAlphaIndex a = if asciiSmallLetter (fromEnum a) then (fromEnum a) - 97 else (fromEnum a) - 65
-
-filterWhitespace :: [Char] -> [Char]
-filterWhitespace = filter (\a -> fromEnum a /= 32)
-
 encryptVigenere :: [Char] -> [Char] -> [Char]
-encryptVigenere word key = groupFive ([encrypt x y | (x, y) <- zip (filterWhitespace word) (cycle key)])
+encryptVigenere word key = groupFive ([encryptVig x y | (x, y) <- zip (filterWhitespace word) (cycle key)])
 
-encrypt x y | asciiSmallLetter   (fromEnum x) = toEnum (mod (toAlphaIndex x + toAlphaIndex y) 26 + 97)
+encryptVig x y | asciiSmallLetter   (fromEnum x) = toEnum (mod (toAlphaIndex x + toAlphaIndex y) 26 + 97)
             | asciiCapitalLetter (fromEnum x) = toEnum (mod (toAlphaIndex x + toAlphaIndex y) 26 + 65)
             | otherwise = x
 decryptVigenere :: [Char] -> [Char] -> [Char]
-decryptVigenere word key = [decrypt x y | (x, y) <- zip (filterWhitespace word) (cycle  key)]
+decryptVigenere word key = [decryptVig x y | (x, y) <- zip (filterWhitespace word) (cycle  key)]
 
-decrypt x y | asciiSmallLetter   (fromEnum x) = toEnum (mod (toAlphaIndex x - toAlphaIndex y) 26 + 97)
+decryptVig x y | asciiSmallLetter   (fromEnum x) = toEnum (mod (toAlphaIndex x - toAlphaIndex y) 26 + 97)
             | asciiCapitalLetter (fromEnum x) = toEnum (mod (toAlphaIndex x - toAlphaIndex y) 26 + 65)
             | otherwise = x
