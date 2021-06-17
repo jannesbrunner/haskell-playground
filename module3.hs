de3973f702db46e7d69669f00c1ac50517172361
Jannes Brunner <jannes.brunner@oo-services.com>
2021-06-17 11:37:37 +0200

    implement (!) get operator

--- module3.hs
+++ module3.hs
@@ -7,3 +7,7 @@ data IntervalMap k v = IntervalMap {keys :: [k] , values :: [v]} |Empty deriving
 
 singleton :: (Enum k, Num k) => v -> IntervalMap k v
 singleton v = IntervalMap{keys=[0..], values= repeat v}
+
+-- get operator => a ! 5 = value at position 5
+(!) :: Ord k => IntervalMap k v -> k -> v
+(!) iMap k = snd (head (filter (\(x, y) -> x == k) (zip (keys iMap) (values iMap)) ))
